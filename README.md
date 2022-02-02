f45df3e787725a27e44596aa1866721bcc476937
Auctioneer99 <rus_zalil_99@mail.ru>
2022-02-02 16:17:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ClayMarket
+ 
---
+++ server/src/main/java/com/example/demo/domain/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/greet")
+    public String get() {
+        return "Hello!";
+    }
+}
