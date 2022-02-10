445f3e659945bf5944bd6c87ec942995f529b460
Auctioneer99 <rus_zalil_99@mail.ru>
2022-02-10 22:17:09 +0500

    feat: update authService

--- client/src/domain/keycloak.js
+++ client/src/domain/keycloak.js
@@ -1,48 +1,38 @@
 import Keycloak from "keycloak-js";
+import routes from "./routes";
+
+var { host } = routes;
 
 let initOptions = {
   url: `http://localhost:${process.env.KEYCLOAK_PORT ?? 8080}/auth`,
   realm: process.env.KEYCLOAK_REALM ?? "master",
   clientId: process.env.KEYCLOAK_VUE_CLIENT ?? "client",
-  onLoad: "login-required",
+  //onLoad: "check-sso", //"login-required",
 };
 
 let keycloak = Keycloak(initOptions);
 
-keycloak
-  .init({ onLoad: initOptions.onLoad })
-  .then((auth) => {
-    if (auth == false) {
-      window.location.reload();
-    } else {
-      console.log("Authenticated");
+export default {
+  init() {
+    return keycloak.init({ onLoad: initOptions.onLoad });
+  },
+  updateToken() {
+    keycloak.updateToken(70);
+  },
+  login(url) {
+    if (keycloak.authenticated == false) {
+      return keycloak.login({ redirectUri: host + url });
     }
-
-    setInterval(() => {
-      keycloak
-        .updateToken(70)
-        .then((refreshed) => {
-          if (refreshed) {
-            console.log("Token refreshed" + refreshed);
-          } else {
-            console.log(
-              "Token not refreshed, valid for " +
-                Math.round(
-                  keycloak.tokenParsed.exp +
-                    keycloak.timeSkew -
-                    new Date().getTime() / 1000
-                ) +
-                " seconds"
-            );
-          }
-        })
-        .catch(() => {
-          console.log("Failed to refresh token");
-        });
-    }, 6000);
-  })
-  .catch(() => {
-    console.log("Authenticated Failed");
-  });
-
-export default keycloak;
+    this.updateToken();
+  },
+  logout() {
+    keycloak.logout();
+  },
+  register() {
+    keycloak.register();
+    this.updateToken();
+  },
+  getToken() {
+    return "Bearer " + keycloak.token;
+  },
+};
