2616ab6b70ef8dc93a0d9e571e24943c39646382
Auctioneer99 <rus_zalil_99@mail.ru>
2022-02-13 12:24:25 +0500

    feat: adding swagger

--- server/src/main/java/com/example/clayMarket/config/WebConfiguration.java
+++ server/src/main/java/com/example/clayMarket/config/WebConfiguration.java
@@ -1,10 +1,17 @@
 package com.example.clayMarket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurationSupport {
 
     @Override
@@ -13,5 +20,18 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
     ) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
